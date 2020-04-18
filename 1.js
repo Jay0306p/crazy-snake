a1ea9a63271b0052aa78e41305572ee20d550b9e
Jay0306p <56125385+Jay0306p@users.noreply.github.com>
2020-04-18 22:05:37 +0530

    Delete 1.js

--- 1.js
+++
@@ -1,8 +0,0 @@
-var 1stname = prompt("enter your first name");
-var 2ndname = prompt("enter your last name");
-var age = prompt("enter your age");
-
-var fullName = 1stname + " " + 2nd name
-
-console.log("your full name is " + fullName");
-console.log("ypour age is "+ age")
